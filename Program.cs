1fb5b9fa39b79426bf91cd895fff5c3d135f7d4c
Hung-Yang-Shih <d11416204@gs.takming.edu.tw>
2026-04-10 11:14:35 +0800

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,57 @@
+﻿namespace _0410_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Person p = new Person();
+            
+            Console.WriteLine(p.name);
+
+            Person p1 = new Person("帶入後參數的名字");
+
+            Console.WriteLine(p1.name);
+
+            Student std= new Student();
+
+            Console.WriteLine(std.name);
+
+            Student std1 = new Student("主程式帶入參數的名字");
+
+            Console.WriteLine(std1.name);
+        }
+
+
+        public class Person
+        {
+            public string name;
+            public Person()
+            {
+                name = "Person的名字";
+            }
+
+            public Person(string name)
+            {
+                this.name = name;
+             }
+
+        }
+
+
+
+        public class Student : Person
+        {
+            static string name = "std靜態資料型別設字";
+
+            public Student() :base()
+            {
+
+            }
+            public Student(string name)
+            {
+
+            }
+
+        }
+    }
+}
